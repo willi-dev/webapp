ad958736a17e3054356e89eadd62956fb0a563ce
willi-dev <willi.ilmukomputer@gmail.com>
2017-02-21 09:15:40 +0700

    update frontend build system (gulp)

--- assets/gulpfile.js
+++ assets/gulpfile.js
@@ -6,26 +6,29 @@ var loadPlugin = require( 'gulp-load-plugins' )();
 var distDir = 'dist/';
 
 var materializePath = [
-	'sass/components/',
+	'dev/sass/components/',
 ];
 
+// start tasking
 gulp.task( 'start', function(){
 	console.log( 'start task...' );
-})
+});
 
 /*
  * sass processor
  */
 gulp.task('sass-processor', function(){
-	gulp.src( 'sass/materialize.scss')
+	gulp.src( 'dev/sass/materialize.scss')
 		.pipe( loadPlugin.sass(
 			{
 				includePaths: materializePath,
 				errLogToConsole: true,
 				outputStyle: 'extended'
 			}
-		).on('error', loadPlugin.sass.logError) )
-		.pipe( gulp.dest('./css/') );
+		).on( 'error', loadPlugin.sass.logError) )
+		.pipe( gulp.dest( './dev/css/' ) )
+		.pipe( minifyCSS() )
+		.pipe( gulp.dest( './dist/css/' ) );
 });
 
 /*
@@ -36,7 +39,7 @@ gulp.task('sass-processor', function(){
  * watch
  */
 gulp.task( 'watch', ['start', 'sass-processor'], function(){
-	gulp.watch( 'sass/components/**/*.scss', ['sass-processor'] );
+	gulp.watch( 'dev/sass/components/**/*.scss', ['sass-processor'] );
 })
 
 gulp.task( 'default', ['watch'] );
